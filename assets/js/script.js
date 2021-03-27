a42f16508c1731d91f526c7caea7ca165f9acfc4
Sam Oberg <samueloberg@hotmail.com>
2021-03-26 21:17:18 -0700

    contact icons added with animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,49 +8,34 @@ for (var i = 0; i < closeBtn.length; i++) {
     console.log(closeBtn[i])
 };
 
-document.querySelector('#contactBtn').addEventListener('click', function() {
-    mainPage.style.display = 'none';
-    resumeModal.style.display = 'none';
-    projectModal.style.display = 'none';
-    contactModal.style.display = 'block';
-});
-
 document.querySelector('#projectBtn').addEventListener('click', function() {
     mainPage.style.display = 'none';
     resumeModal.style.display = 'none';
-    contactModal.style.display = 'none';
     projectModal.style.display = 'block';
 });
 
 document.querySelector('#resumeBtn').addEventListener('click', function() {
     mainPage.style.display = 'none';
-    contactModal.style.display = 'none';
     projectModal.style.display = 'none';
     resumeModal.style.display = 'block';
 });
 
 closeBtn[0].addEventListener('click', function () {
     resumeModal.style.display = 'none';
-    contactModal.style.display = 'none';
     projectModal.style.display = 'none';
     mainPage.style.display = 'grid';
 });
+
 closeBtn[1].addEventListener('click', function () {
     resumeModal.style.display = 'none';
-    contactModal.style.display = 'none';
     projectModal.style.display = 'none';
     mainPage.style.display = 'grid';
 });
+
 closeBtn[2].addEventListener('click', function () {
     resumeModal.style.display = 'none';
-    contactModal.style.display = 'none';
     projectModal.style.display = 'none';
     mainPage.style.display = 'grid';
 });
-closeBtn[3].addEventListener('click', function () {
-    resumeModal.style.display = 'none';
-    contactModal.style.display = 'none';
-    projectModal.style.display = 'none';
-    mainPage.style.display = 'grid';
-});
+
 
